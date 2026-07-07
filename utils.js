edfc9d29e6c385dec6fb623289d9415a6184deb1
JCRuffino <69386354+JCRuffino@users.noreply.github.com>
2026-07-07 12:39:49 +0100

    Add utility functions for HTML escaping and screen display

---
+++ utils.js
@@ -0,0 +1,13 @@
+function escHtml(str) {
+  return str
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;');
+}
+
+function showScreen(id) {
+  document.querySelectorAll('.screen').forEach(s => s.classList.remove('active'));
+  document.getElementById(id).classList.add('active');
+  window.scrollTo(0, 0);
+}
